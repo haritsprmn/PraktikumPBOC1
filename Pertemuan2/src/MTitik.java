628b3a0c7592fbadf1c1d962cbf5fa39bca93a09
Harits Permana <haritsprmn@gmail.com>
2026-02-26 17:25:09 +0700

    tambahan soal

--- Pertemuan2/src/MTitik.java
+++ Pertemuan2/src/MTitik.java
@@ -6,5 +6,19 @@ public class MTitik {
         T1.printTitik();
         T1.geser(3, 4);
         T1.printTitik();
+        Titik T2 = T1;
+        T2.printTitik();
+        // kalau ga salah karena T2 menyimpan alamat T1 jadi jika T1 di ubah T2 ikut
+        T1.setAbsis(9);
+        T1.setOrdinat(3);
+        T2.printTitik();
+
+        Titik T3 = new Titik();
+        T3.printTitik();
+        Titik T4 = new Titik(7, 2);
+        T4.printTitik();
+
+        System.out.println("Jumlah Objek Titik: " + Titik.getCounterTitik());
+        System.out.println("Jumlah Objek Titik: " + T2.getCounterTitik());
     }
 }
--- Pertemuan2/src/Titik.java
+++ Pertemuan2/src/Titik.java
@@ -1,18 +1,30 @@
 // Nama File    : Titik.java
 // Deskripsi    : Berisi atribut dan method dalam class Titik
 // Pembuat      : Harits Permana
-// Tanggal      : 19/02/2026
+// Tanggal      : 26/02/2026
 
 public class Titik {
     /****************** ATRIBUT *********************/
     double absis;
     double ordinat;
+    static int counterTitik = 0;
 
     /****************** METHOD ****************************/
     // Konstruktor untuk membuat titik (0,0)
     Titik() {
         absis = 0;
         ordinat = 0;
+        counterTitik++;
+    }
+
+    // overloading
+    Titik(double x, double y) {
+        absis = x;
+        ordinat = y;
+    }
+
+    static int getCounterTitik() {
+        return counterTitik;
     }
 
     // mengembalikan nilai absis
